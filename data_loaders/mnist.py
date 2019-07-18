1febda66ec24ae27397bda3561849d5db6e302d9
Daniel Watson <daniel.watson@nyu.edu>
2019-07-18 15:36:46 -0700

    working examples

--- data_loaders/mnist.py
+++ data_loaders/mnist.py
@@ -11,11 +11,21 @@ class MNIST(tfbp.DataLoader):
         train_data, test_data = tf.keras.datasets.mnist.load_data()
 
         test_data = tf.data.Dataset.from_tensor_slices(test_data)
-        test_data = test_data.batch(self.hparams.batch_size)
+        test_data = self._transform_dataset(test_data)
 
-        if self.training:
+        if self.method in ["fit", "train"]:
             train_data = tf.data.Dataset.from_tensor_slices(train_data)
-            train_data = train_data.batch(self.hparams.batch_size)
+            train_data = self._transform_dataset(train_data)
             return train_data, test_data
 
         return test_data
+
+    def _transform_dataset(self, dataset):
+        dataset = dataset.shuffle(10000)
+        dataset = dataset.batch(self.hparams.batch_size)
+        return dataset.map(
+            lambda x, y: (
+                tf.reshape(tf.cast(x, tf.float32), [-1, 28 * 28]),
+                tf.cast(y, tf.int64),
+            )
+        )
--- models/mlp.py
+++ models/mlp.py
@@ -6,7 +6,11 @@ import boilerplate as tfbp
 
 @tfbp.default_export
 class MLP(tfbp.Model):
-    default_hparams = {"layer_sizes": [100, 10], "learning_rate": 0.1}
+    default_hparams = {
+        "layer_sizes": [512, 10],
+        "learning_rate": 0.001,
+        "num_epochs": 10,
+    }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,38 +22,63 @@ class MLP(tfbp.Model):
         )
 
     def call(self, x):
-        if self.training:
-            x, y = x
+        return self.forward(x)
 
-        x = self.forward(x)
-        outputs = tf.argmax(x, axis=-1)
-        accuracy = tf.reduce_mean(tf.cast(tf.equal(outputs, y), tf.float32))
+    def fit(self, data_loader):
+        """Example using keras training loop."""
+        train_data, valid_data = data_loader.load()
 
-        if not self.training:
-            return outputs, accuracy
+        loss_fn = tf.losses.SparseCategoricalCrossentropy()
+        optimizer = tf.optimizers.Adam(self.hparams.learning_rate)
 
-        loss = tf.losses.CategoricalCrossentropy(labels=y, logits=x)
-        return loss, outputs, accuracy
-
-    def fit(self, dataset):
-        step = 0
-        train_data, _ = dataset
-
-        optimizer = tf.optimizers.SGD(self.hparams.learning_rate)
-
-        for batch in train_data:
-            if not self.built:
-                self.build([tf.shape(batch[0]), tf.shape(batch[1])])
-
-            with tf.GradientTape() as g:
-                loss, outputs, accuracy = self(batch)
-            grads = g.gradients(loss, self.trainable_variables)
-            optimizer.apply_gradients(zip(grads, self.trainable_variables))
-
-            print(f"step {step} (train_loss={loss})")
-
-            if self.step % 1000 == 0:
-                tf.summary.scalar("loss", loss)
-                tf.summary.scalar("accuracy", accuracy)
-                self.save()
+        self.compile(optimizer, loss_fn)
+        super().fit(
+            x=train_data,
+            validation_data=valid_data,
+            validation_steps=1,
+            validation_freq=1,
+            epochs=self.hparams.num_epochs,
+            shuffle=False,
+        )
         self.save()
+
+    def train(self, data_loader):
+        """Example using custom training loop."""
+        step = 0
+        train_data, valid_data = data_loader.load()
+
+        # Allow to call `next` builtin indefinitely.
+        valid_data = iter(valid_data.repeat())
+
+        loss_fn = tf.losses.SparseCategoricalCrossentropy()
+        optimizer = tf.optimizers.Adam(self.hparams.learning_rate)
+
+        for epoch in range(self.hparams.num_epochs):
+            for x, y in train_data:
+
+                with tf.GradientTape() as g:
+                    loss = loss_fn(y, self(x))
+
+                grads = g.gradient(loss, self.trainable_variables)
+                optimizer.apply_gradients(zip(grads, self.trainable_variables))
+
+                if step % 1000 == 0:
+                    print(f"step {step} (train_loss={loss})")
+                step += 1
+
+            # Save and validate at the end of every epoch.
+            x, y = next(valid_data)
+            loss = loss_fn(y, self(x))
+            print(f"epoch {epoch} finished (valid_loss={loss})")
+            self.save()
+
+    def evaluate(self, data_loader):
+        n = 0
+        accuracy = 0
+        test_data = data_loader.load()
+        for x, y in test_data:
+            true_pos = tf.math.equal(y, tf.math.argmax(self(x), axis=-1))
+            for i in true_pos.numpy():
+                n += 1
+                accuracy += (i - accuracy) / n
+        print(accuracy)
